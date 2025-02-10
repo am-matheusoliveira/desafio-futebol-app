eba25eb061d69ce3fd32917ad1faeaae42a5fae3
Matheus Pereira <matheus.pereira@prumoengenharia.com.br>
2025-02-10 09:15:22 -0300

    Primeiro Commit - Start

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,10 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    safelist: [
+        'bg-red-500', 'border-red-400', 'bg-red-100', 'text-red-700',
+        'bg-yellow-500', 'border-yellow-400', 'bg-yellow-100', 'text-yellow-700'
+    ],
     theme: {
         extend: {
             fontFamily: {
